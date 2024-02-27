85d5f0fb7bab5631d8d7eb1fdca7f7b25125aad6
aryajeet66 <shindearyajeet@gmail.com>
2024-02-27 10:16:59 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  //var dec.
+  public showMyMessage = false
+  public msg:string = "Hello...."
   title = 'demo';
+  //fun dec.
+  callMsg(){
+    console.log("clicked")
+    setTimeout(() => {
+      this.showMyMessage = true;
+  }, 3000);
+  }
 }
+
